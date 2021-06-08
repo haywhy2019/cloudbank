8b4cf9e882c72a88a9fd7f5c06fa16e7bd5bc8f6
haywhy2019 <olusegun.sola19@gmail.com>
2021-06-08 22:59:10 +0100

    add all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.scss';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Layout from "./components/Layout/Layout"
+import Invest from "./views/Investment/Investment"
+import Dashboard from "./views/Dashboard/Dashboard"
+import Validation from "./views/Validation/validation"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Router>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/invest" component={Invest} />
+        <Route path="/validation" component={Validation} />
+      </Switch>
+    </Router>
     </div>
   );
 }
